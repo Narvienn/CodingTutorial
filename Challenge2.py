f41972cd21daf24cd0506b6126b3087a0b95b61e
zwachocka <zwachocka@andea.com>
2019-10-29 15:53:18 +0100

    First commit

---
+++ Challenge2.py
@@ -0,0 +1,8 @@
+"""A flight company has a library of films to show its customers on long flights. Design a system that would select
+exactly two films per flight based on the duration of the flight - the running time of both films needs to be shorter
+or equal to the flight duration."""
+
+# target = flight times
+# def function(library of films, target):
+
+# aim for a solution of n*n solution or better (e.g. for x for y)
