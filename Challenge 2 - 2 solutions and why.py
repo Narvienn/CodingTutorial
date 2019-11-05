30bd4c6b1da409aca7019731330300c725d9218b
zwachocka <zwachocka@andea.com>
2019-11-05 19:33:00 +0100

    Trying solution #1

---
+++ Challenge 2 - 2 solutions and why.py
@@ -0,0 +1,30 @@
+"""A flight company has a library of films to show its customers on long flights. Design a system that would select
+exactly two films per flight based on the duration of the flight - the running time of both films needs to be
+equal to the flight duration."""
+# target = flight times
+# def solution(library of films, target)
+# aim for a solution of n*n solution or better (e.g. for x for y
+
+
+flight_time = 180 # a list of flight lengths in minutes
+films = [40, 60, 80, 90, 100, 110, 115] # a list of film lengths in minutes
+
+# 30, 30 - 60 --> that means film1 == film2, which we need to avoid
+
+
+def solution(films, target):  # O(n*n)
+    for index1, f1 in enumerate(films): # enumerate: built-in function that returns counter? + value of item in list
+        for index2, f2 in enumerate(films):
+            if f1 + f2 == target and not (index1 is index2):
+                return f1, f2
+    return [] # Q: What are we returning here if return f1, f2 is already defined?
+
+
+def solution2(films, target):
+    flights = { }
+    for f1 in films: # O(n)
+        if f1 not in flights:
+            flights[f1] = target - f1
+        else:
+            return f1, target - f1
+    return []
