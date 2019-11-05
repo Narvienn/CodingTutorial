2014b1e4f67c722c2c63f28fac839c41996d3a1a
zwachocka <zwachocka@andea.com>
2019-11-05 19:38:00 +0100

    Commenting the solutions (enumerate + function)

--- Challenge 2 - 2 solutions and why.py
+++ Challenge 2 - 2 solutions and why.py
@@ -6,14 +6,14 @@ equal to the flight duration."""
 # aim for a solution of n*n solution or better (e.g. for x for y
 
 
-flight_time = 180 # a list of flight lengths in minutes
+flight_time = 180 # just one flight - once there is a function, we can run it on however many flight time instances
 films = [40, 60, 80, 90, 100, 110, 115] # a list of film lengths in minutes
 
 # 30, 30 - 60 --> that means film1 == film2, which we need to avoid
 
 
 def solution(films, target):  # O(n*n)
-    for index1, f1 in enumerate(films): # enumerate: built-in function that returns counter? + value of item in list
+    for index1, f1 in enumerate(films): # enumerate: built-in function that returns value of item and counter in list
         for index2, f2 in enumerate(films):
             if f1 + f2 == target and not (index1 is index2):
                 return f1, f2
