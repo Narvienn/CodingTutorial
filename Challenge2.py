07f26c5be7e1a8ee962e04eea2fbb005de54acf6
zwachocka <zwachocka@andea.com>
2019-11-12 12:14:01 +0100

    Cleaning the code

--- Challenge2.py
+++ Challenge2.py
@@ -2,24 +2,23 @@
 exactly two films per flight based on the duration of the flight - the running time of both films needs to be shorter
 or equal to the flight duration."""
 # target = flight times
-# def function(library of films, target):
+# def function(library of films, target) <-- WHY this function + these arguments?
 # aim for a solution of n*n solution or better (e.g. for x for y)
+
 
 import random
 
 flight_times = [180, 240, 280, 360, 440, 550] # a list of flight lengths in minutes
 films = [40, 60, 85, 90, 100, 110, 115] # a list of film lengths in minutes
 
-flight = random.choices(flight_times)
-print(flight)
+film = random.choice(films)
+film2 = []
 
-film1 = random.choice(films) # randomly selects the first film to display
-film2 = int # placeholder for 'an unspecified item from films'
 
-# film1 != film2 # we need to ensure that one film is not randomly selected twice
+# film1 != film2
+# Q2: Where + How to insert this condition? try/except? while?
 
-# def suggest_films(flight_times, films):
 for flight in flight_times:
     for film in films:
-        if film1 + film2 =< flight:
-            print(film1, film2)
+        # iterate over films to find a film2 value so that film + film2 <= flight
+        #Q: Unacceptable because that makes it O(n*3) complexity
